764cfdeb3e1612d29e32bd8cbd38c4303aff4962
fabiocalheiros <fabio.calheiros@gmail.com>
2019-10-02 14:22:40 -0300

    change class to hooks

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -11,31 +11,12 @@ import * as CartActions from '../../store/modules/cart/actions';
 
 import { ProductList } from './styles';
 
-class Home extends Component {
-  state = {
-    category: 'MLB3707',
-    products: [],
-    offset: 1,
-  };
+function Home({amount, addToCartRequest}) {
+  const [products, setProducts] = useState([]);
+  const [category, setCategory] = useState('MLB1051');
+  const [offset, setOffset] = useState(1);
 
-  async componentDidMount() {
-    const { category } = this.state;
-
-    const response = await api.get(`/sites/MLB/search?category=${category}`);
-
-    console.tron.log(response);
-
-    const data = response.data.results.map(product => ({
-      ...product,
-      priceFormatted: formatPrice(product.price),
-    }));
-
-    this.setState({ products: data });
-  }
-
-  loadItens = async () => {
-    const { offset, category } = this.state;
-
+  async function loadItens() {
     const response = await api.get(`/sites/MLB/search?category=${category}`, {
       params: {
         offset: offset * 50,
@@ -47,60 +28,51 @@ class Home extends Component {
       priceFormatted: formatPrice(product.price),
     }));
 
-    this.setState({ products: data });
-  };
-
-  handlePage = async (current, pageSize) => {
-    await this.setState({
-      offset: current,
-    });
-    this.loadItens();
-    window.scrollTo(0, 0);
-  };
-
-  handleAddProduct = product => {
-    const { addToCartRequest } = this.props;
-
-    addToCartRequest(product);
-  };
-
-  render() {
-    const { products } = this.state;
-    const { amount } = this.props;
-
-    return (
-      <>
-        <ProductList>
-          {products.map(product => (
-            <li key={product.id}>
-              <Link to={`/product/${product.id}`}>
-                <img src={product.thumbnail} alt={product.title} />
-              </Link>
-              <strong>{product.title}</strong>
-              <span>{product.priceFormatted}</span>
-              <button
-                type="button"
-                onClick={() => this.handleAddProduct(product)}
-              >
-                <div>
-                  <MdAddShoppingCart size={16} color="#fff" />
-                  {amount[product.id] || 0}
-                </div>
-                <span>ADICIONAR AO CARRINHO</span>
-              </button>
-            </li>
-          ))}
-        </ProductList>
-        <Pagination onChange={this.handlePage} defaultCurrent={1} total={200} />
-      </>
-    );
+    setProducts(data);
   }
+
+  useEffect(() => {
+    loadItens();
+  }, []);
+
+  function handlePage(current, pageSize) {
+    setOffset(current);
+    loadItens();
+    window.scrollTo(0, 0);
+  }
+
+  function handleAddProduct(id) {
+    addToCartRequest(id);
+  }
+
+  return (
+    <>
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <Link to={`/product/${product.id}`}>
+              <img src={product.thumbnail} alt={product.title} />
+            </Link>
+            <strong>{product.title}</strong>
+            <span>{product.priceFormatted}</span>
+            <button type="button" onClick={() => handleAddProduct(product)}>
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" />
+                {amount[product.id] || 0}
+              </div>
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+      <Pagination onChange={handlePage} defaultCurrent={1} total={200} />
+    </>
+  );
 }
 
 const mapStateToProps = state => ({
   amount: state.cart.reduce((amount, product) => {
     amount[product.id] = product.amount;
-
     return amount;
   }, {}),
 });
